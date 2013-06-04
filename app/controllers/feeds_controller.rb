3018274bc5b4513f1533f646d0ad9612cdff9a65
Hector Reyes Aleman <hector.rad18@gmail.com>
2013-06-04 01:06:47 -0300

    Update feeds_controller.rb

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,8 +6,31 @@ require 'feedzirra'
 
 
 class FeedsController < ApplicationController
+  
+  MAX_FEED_ITEMS = 15
+  Currentfeed = Struct.new(:feed_url, :feed_title, :title, :url, :published, :summary)
+
   def index
     @feeds = Feed.all
+    @mem_feeds = Array.new()
+    
+    i = 0
+    @feeds.each do |feed|
+        f = Feedzirra::Feed.fetch_and_parse(feed.url)        
+        feed_url = f.url
+        feed_title = f.title
+        f.entries.each do |item|
+            title     = item.title
+            url       = item.url
+            published = item.published 
+            summary   = item.summary
+            @mem_feeds[i] = Currentfeed.new(feed_url, feed_title, title, url, published, summary)
+            break
+        end
+        i+=1
+        break if i>=MAX_FEED_ITEMS
+    end
+
   end
   
   def show
@@ -45,7 +68,7 @@ class FeedsController < ApplicationController
   
   def destroy
     Feed.find(params[:id]).destroy
-    flash[:notice] = "Feed Deleted."
+    flash[:notice] = "Ambulancia Eliminada."
     redirect_to :action => 'index'
   end
 end
