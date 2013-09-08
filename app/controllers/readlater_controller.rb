74278e5cc450cce8ae78c202aa7967393dcaa265
Hector Reyes Aleman <hector@treize.(none)>
2013-09-07 23:02:21 -0500

    a

--- app/controllers/readlater_controller.rb
+++ app/controllers/readlater_controller.rb
@@ -9,7 +9,7 @@ class ReadlaterController < ApplicationController
     @item.user_id = session[:user_id]
     @item.save
     flash[:notice] = "Item added to read later list."
-    redirect_to "/lire/feeds/#{params[:readlater][:feed_id]}\##{params[:feed_item_id]}"
+    redirect_to :action => 'index'#"/lire/feeds/#{params[:readlater][:feed_id]}\##{params[:feed_item_id]}"
   end
   
   def destroy
